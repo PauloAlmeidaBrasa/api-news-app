24fb3fb57cdcd87b40ccd94d8e8d251e475aec8f
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2026-02-23 11:47:12 -0300

    fat: adding aliases path

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,15 @@ import express from "express";
 // import { errorHandler } from "./middleware/errorMiddleware"
 // import { corsMiddleware } from "./middleware/corsMiddleware";
 // import registerRouter from "@routes/index";
-// import { Knex } from "knex";
+import { Knex } from "knex";
 
 // db: Knex
-export const createApp = () => {
+export const createApp = (db: Knex) => {
   const app = express();
 
   console.log('testing pipeline, testing only')
 
-  /// console.log("🧪 DB IN APPLICATION:", db);
+  // console.log("🧪 DB IN APPLICATION:", db);
 
 
   app.use(express.json());
