e503d6fca91948387fcfb2826db3ff7ac48a3af8
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2026-02-24 11:18:20 -0300

    fix: removing comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
 import newsRoutes from "@routes/newsRouter";
-// import authenticationRoutes from "@routes/authenticationRouter"
-// import { authMiddleware } from "middleware/authMiddleware"
 import { Knex } from "knex";
-// import docRoutes from "./docRouter";
-
 
 const registerRouter = (db: Knex) => {
 
@@ -12,13 +8,6 @@ const registerRouter = (db: Knex) => {
   const API_VERSION = process.env.API_VERSION || "v1"
 
   router.use(`/${API_VERSION}`,newsRoutes(db)) //public routes
-  // router.use(`/${API_VERSION}`,docRoutes())
-
-
-  // router.use(`/${API_VERSION}`,authMiddleware,userRoutes(db)) //auth routes
-  // router.use(`/${API_VERSION}`,authMiddleware,clientRoutes(db))
-  // router.use(`/${API_VERSION}`,authMiddleware,categoryRoutes(db))
-
 
 
   return router
